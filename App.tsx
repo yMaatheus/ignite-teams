9aa740b9a41ebe5a0b733a8651cc81a77c53724e
¨Matheus¨ <ymatheus.h@gmail.com>
2023-05-28 14:15:38 -0300

    feat: :sparkles: create app theme and implements theme provider in App

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { Groups } from '@screens/Groups';
 import { StatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/theme';
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Groups />
       <StatusBar style="auto" />
-    </>
+    </ThemeProvider>
   );
 }
