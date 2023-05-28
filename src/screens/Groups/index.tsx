98bee45e8c3b4270d4f56f01e1d3e82c521bbfee
¨Matheus¨ <ymatheus.h@gmail.com>
2023-05-28 15:14:50 -0300

    feat: :sparkles: implements component HightLight from Groups screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,15 @@
 import { Header } from '@components/Header'
+import { HightLight } from '@components/HightLight'
 import { Container } from './styles'
 
 export function Groups() {
-  return(
+  return (
     <Container>
       <Header />
+      <HightLight
+        title='Turmas'
+        subtitle='jogue com a sua turma'
+      />
     </Container>
   )
 }
