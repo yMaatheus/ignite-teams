d29165e2b6d6e6b6bb5ab7a58ac0894905164ac8
¨Matheus¨ <ymatheus.h@gmail.com>
2023-05-28 20:35:31 -0300

    feat: :sparkles: implement FlatList with GroupCard component in Groups screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,37 @@
+import { GroupCard } from '@components/GroupCard'
 import { Header } from '@components/Header'
 import { HightLight } from '@components/HightLight'
+import { ListEmpty } from '@components/ListEmpty'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 import { Container } from './styles'
 
 export function Groups() {
+  const [groups, setGroups] = useState([]);
+
   return (
     <Container>
       <Header />
+
       <HightLight
         title='Turmas'
         subtitle='jogue com a sua turma'
+      />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={ groups.length === 0 && { flex: 1 } }
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message='Que tal cadastrar a primeira turma?'
+          />
+        )}
       />
     </Container>
   )
