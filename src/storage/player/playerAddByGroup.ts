50a2e52014d921575e6d52860477bb31f817794f
Matheus <ymatheus.h@gmail.com>
2023-09-20 15:58:03 -0300

    refactor: :truck: rename file playerGetByGroup to playersGetByGroup

--- src/storage/player/playerAddByGroup.ts
+++ src/storage/player/playerAddByGroup.ts
@@ -2,7 +2,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { AppError } from "@utils/AppError";
 
 import { PLAYER_COLLECTION } from "@storage/storageConfig";
-import { playersGetByGroup } from './playerGetByGroup'
+import { playersGetByGroup } from './playersGetByGroup'
 import { PlayerStorageDTO } from './PlayerStorageDTO'
 
 export async function playerAddByGroup(newPlayer: PlayerStorageDTO, group: string,) {
