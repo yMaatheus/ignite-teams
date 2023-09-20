5da7ac4d257cc02689f8e198a3f4bd4bd39e2c25
Matheus <ymatheus.h@gmail.com>
2023-09-20 15:46:16 -0300

    feat: :sparkles: implements player add by group and handle custom errors

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -11,17 +11,46 @@ import { Button } from "@components/Button";
 import { ListEmpty } from "@components/ListEmpty";
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import { useRoute } from "@react-navigation/native";
+import { Alert } from "react-native";
+import { AppError } from "@utils/AppError";
+import { playerAddByGroup } from "@storage/player/playerAddByGroup";
+import { playersGetByGroup } from "@storage/player/playerGetByGroup";
 
 type RouteParams = {
   group: string;
 }
 
 export function Players() {
+  const [newPlayerName, setNewPlayerName] = useState('');
   const [team, setTeam] = useState('Time A');
   const [players, setPlayers] = useState([])
 
   const route = useRoute();
   const { group } = route.params as RouteParams;
+
+  async function handleAddPlayer() {
+    if (newPlayerName.trim().length === 0) {
+      return Alert.alert("Novo Jogador", "Digite o nome do jogador para adicionar");
+    }
+
+    const newPlayer = {
+      name: newPlayerName,
+      team
+    }
+
+    try {
+
+      await playerAddByGroup(newPlayer, group)
+
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert("Novo Jogador", error.message);
+      } else {
+        console.log(error)
+        Alert.alert("Novo Jogador", "Não foi possível adicionar o jogador.");
+      }
+    }
+  }
 
   return (
     <Container>
@@ -34,11 +63,15 @@ export function Players() {
 
       <Form>
         <Input
+          onChangeText={setNewPlayerName}
           placeholder="Nome da pessoa"
           autoCorrect={false}
         />
 
-        <ButtonIcon icon="add" />
+        <ButtonIcon
+          icon="add"
+          onPress={handleAddPlayer}
+        />
       </Form>
 
       <HeaderList>
